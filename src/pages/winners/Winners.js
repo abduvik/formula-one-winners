7d6df565825860b49729df144cc44a0a8e25253b
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-16 01:03:54 +0200

    Add back to home link

--- src/pages/winners/Winners.js
+++ src/pages/winners/Winners.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import "./Winners.scss";
 import WinnersList from "../../components/winners-list/WinnersList";
@@ -8,15 +9,20 @@ export class Winners extends Component {
     super(props);
     this.state = {
       loaded: false,
-      year: this.props.match.params.year,
+      year: this.props.match.params.year
     };
   }
 
   render() {
     return (
       <div className="ui container winners-page">
-        <h1>{this.state.year} Winners</h1>
         <div className="ui four column grid centered align">
+          <div className="two column row">
+            <div className="five column">
+              <Link to="/">&larr; Go Back</Link>
+              <h1>{this.state.year} Winners</h1>
+            </div>
+          </div>
           <div className="two column row">
             <div className="ui segments five column">
               <WinnersList year={this.state.year} />
