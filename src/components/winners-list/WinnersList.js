aad8593f051aa7dc0373b1f91fa8734fa95b3bb4
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-15 23:56:50 +0200

    Winners Page

---
+++ src/components/winners-list/WinnersList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class WinnersList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
--- src/pages/winners/Winners.js
+++ src/pages/winners/Winners.js
@@ -1,7 +1,54 @@
 import React, { Component } from "react";
+import * as ErgastAPI from "./../../services/ErgastAPI";
+
+import "./Winners.scss";
 
 export class Winners extends Component {
-    render(){
-        return (<div>Winners page</div>)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      year: this.props.match.params.year,
+      champion: {},
+      winners: []
+    };
+  }
+
+  componentWillMount() {
+    Promise.all([ErgastAPI.getWorldChampion(this.state.year), ErgastAPI.getWinners(this.state.year)]).then(res => {
+      this.setState({
+        champion: res[0].MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver,
+        winners: res[1].MRData.RaceTable.Races
+      });
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container winners-page">
+        <h1>{this.state.year} Winners</h1>
+        <div className="ui four column grid centered align">
+          <div className="two column row">
+            <div className="ui segments five column">
+              <div className="ui segment">
+                <p>Top</p>
+              </div>
+              <div className="ui segment">
+                <p>Middle</p>
+              </div>
+              <div className="ui segment">
+                <p>Middle</p>
+              </div>
+              <div className="ui segment">
+                <p>Middle</p>
+              </div>
+              <div className="ui segment">
+                <p>Bottom</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
