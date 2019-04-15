234ca784e465bb5b24b99080068e7e46df2a6d22
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-16 00:14:21 +0200

    Created the winners list

---
+++ src/components/winner-row/WinnerRow.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class WinnerRow extends Component {
+  render() {
+    return (
+      <div className="ui segment">
+        <p>Top</p>
+      </div>
+    );
+  }
+}
--- src/pages/winners/Winners.js
+++ src/pages/winners/Winners.js
@@ -2,26 +2,47 @@ import React, { Component } from "react";
 import * as ErgastAPI from "./../../services/ErgastAPI";
 
 import "./Winners.scss";
+import WinnerRow from "../../components/winner-row/WinnerRow";
 
 export class Winners extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loaded: false,
       year: this.props.match.params.year,
-      champion: {},
-      winners: []
+      races: []
     };
   }
 
   componentWillMount() {
     Promise.all([ErgastAPI.getWorldChampion(this.state.year), ErgastAPI.getWinners(this.state.year)]).then(res => {
-      this.setState({
-        champion: res[0].MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver,
-        winners: res[1].MRData.RaceTable.Races
+      // Get the Data
+      let champion = res[0].MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver;
+      let races = res[1].MRData.RaceTable.Races;
+
+      // Flag the World Champion;
+      races = races.map(race => {
+        if (race.Results[0].Driver.driverId === champion.driverId) {
+          race.Results[0].Driver.champion = true;
+        } else {
+          race.Results[0].Driver.champion = false;
+        }
+        return race;
       });
+
+      // Set state of the data
+      this.setState({
+        loaded: true,
+        races: races
+      });
+
       console.log(this.state);
     });
   }
+
+  generateWinnersRow = () => {
+    return this.state.races.map(race => <WinnerRow key={race.raceName} race={race} />);
+  };
 
   render() {
     return (
@@ -30,21 +51,14 @@ export class Winners extends Component {
         <div className="ui four column grid centered align">
           <div className="two column row">
             <div className="ui segments five column">
-              <div className="ui segment">
-                <p>Top</p>
-              </div>
-              <div className="ui segment">
-                <p>Middle</p>
-              </div>
-              <div className="ui segment">
-                <p>Middle</p>
-              </div>
-              <div className="ui segment">
-                <p>Middle</p>
-              </div>
-              <div className="ui segment">
-                <p>Bottom</p>
-              </div>
+              {this.state.loaded ? (
+                this.generateWinnersRow()
+              ) : (
+                <div className="ui segment">
+                  <img className="ui wireframe image" src="/assets/short-paragraph.png" alt="loading paragraph" />
+                  <div className="ui active loader" />
+                </div>
+              )}
             </div>
           </div>
         </div>
