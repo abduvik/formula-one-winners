38710b64a687770bb2f9580b3e093b02202431f3
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-16 00:20:17 +0200

    Moved code from winner page to winner list component

--- src/components/winners-list/WinnersList.js
+++ src/components/winners-list/WinnersList.js
@@ -1,11 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import * as ErgastAPI from "./../../services/ErgastAPI";
+import WinnerRow from "../../components/winner-row/WinnerRow";
 
 export default class WinnersList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      year: this.props.year,
+      races: []
+    };
+  }
+
+  componentWillMount() {
+    Promise.all([ErgastAPI.getWorldChampion(this.state.year), ErgastAPI.getWinners(this.state.year)]).then(res => {
+      // Get the Data
+      let champion = res[0].MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver;
+      let races = res[1].MRData.RaceTable.Races;
+
+      // Flag the World Champion;
+      races = races.map(race => {
+        if (race.Results[0].Driver.driverId === champion.driverId) {
+          race.Results[0].Driver.champion = true;
+        } else {
+          race.Results[0].Driver.champion = false;
+        }
+        return race;
+      });
+
+      // Set state of the data
+      this.setState({
+        loaded: true,
+        races: races
+      });
+    });
+  }
+
+  generateWinnersRow = () => {
+    return this.state.races.map(race => <WinnerRow key={race.raceName} race={race} />);
+  };
+
   render() {
-    return (
-      <div>
-        
+    return this.state.loaded ? (
+      this.generateWinnersRow()
+    ) : (
+      <div className="ui segment">
+        <img className="ui wireframe image" src="/assets/short-paragraph.png" alt="loading paragraph" />
+        <div className="ui active loader" />
       </div>
-    )
+    );
   }
 }
--- src/pages/winners/Winners.js
+++ src/pages/winners/Winners.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-import * as ErgastAPI from "./../../services/ErgastAPI";
 
 import "./Winners.scss";
-import WinnerRow from "../../components/winner-row/WinnerRow";
+import WinnersList from "../../components/winners-list/WinnersList";
 
 export class Winners extends Component {
   constructor(props) {
@@ -14,36 +13,6 @@ export class Winners extends Component {
     };
   }
 
-  componentWillMount() {
-    Promise.all([ErgastAPI.getWorldChampion(this.state.year), ErgastAPI.getWinners(this.state.year)]).then(res => {
-      // Get the Data
-      let champion = res[0].MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver;
-      let races = res[1].MRData.RaceTable.Races;
-
-      // Flag the World Champion;
-      races = races.map(race => {
-        if (race.Results[0].Driver.driverId === champion.driverId) {
-          race.Results[0].Driver.champion = true;
-        } else {
-          race.Results[0].Driver.champion = false;
-        }
-        return race;
-      });
-
-      // Set state of the data
-      this.setState({
-        loaded: true,
-        races: races
-      });
-
-      console.log(this.state);
-    });
-  }
-
-  generateWinnersRow = () => {
-    return this.state.races.map(race => <WinnerRow key={race.raceName} race={race} />);
-  };
-
   render() {
     return (
       <div className="ui container winners-page">
@@ -51,14 +20,7 @@ export class Winners extends Component {
         <div className="ui four column grid centered align">
           <div className="two column row">
             <div className="ui segments five column">
-              {this.state.loaded ? (
-                this.generateWinnersRow()
-              ) : (
-                <div className="ui segment">
-                  <img className="ui wireframe image" src="/assets/short-paragraph.png" alt="loading paragraph" />
-                  <div className="ui active loader" />
-                </div>
-              )}
+              <WinnersList year={this.state.year} />
             </div>
           </div>
         </div>
