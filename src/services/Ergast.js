6a55904a2cb7bc812c1f06160265876cada23d6b
AbdelrahmanSE <abdelrahmanse@flexnebula.com>
2019-04-15 23:33:51 +0200

    Added Year variable support

--- src/services/Ergast.js
+++ src/services/Ergast.js
@@ -3,9 +3,9 @@
  */
 
 // Get the winners array
-export const getWinners = () => 
-  fetch("https://ergast.com/api/f1/2005/results/1.json").then(res => res.json());
+export const getWinners = (year) => 
+  fetch(`https://ergast.com/api/f1/${year}/results/1.json`).then(res => res.json());
 
 // Get the winners array
-export const getWorldChampion = () =>
-  fetch("http://ergast.com/api/f1/2005/driverStandings/1.json").then(res => res.json());
+export const getWorldChampion = (year) =>
+  fetch(`http://ergast.com/api/f1/${year}/driverStandings/1.json`).then(res => res.json());
